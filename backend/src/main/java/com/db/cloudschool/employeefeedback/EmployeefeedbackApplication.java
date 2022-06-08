4e98616577e9a041bc9b6bc05c68f074cd7c6e02
Wiliam Toader <wiliamtoader@protonmail.com>
2022-06-08 16:49:22 +0300

    Initialized Spring Boot project

---
+++ backend/src/main/java/com/db/cloudschool/employeefeedback/EmployeefeedbackApplication.java
@@ -0,0 +1,13 @@
+package com.db.cloudschool.employeefeedback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeefeedbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeefeedbackApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/db/cloudschool/employeefeedback/EmployeefeedbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.db.cloudschool.employeefeedback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeefeedbackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
