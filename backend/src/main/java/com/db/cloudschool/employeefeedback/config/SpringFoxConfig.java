4a3d5760a26db6f0eff04441c9b8b265ba460965
Wiliam Toader <wiliamtoader@protonmail.com>
2022-06-27 04:10:39 +0300

    Set up Swagger UI and created SecurityConfig

---
+++ backend/src/main/java/com/db/cloudschool/employeefeedback/config/SpringFoxConfig.java
@@ -0,0 +1,21 @@
+package com.db.cloudschool.employeefeedback.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SpringFoxConfig {
+    @Bean
+    public Docket api() {
+        // Creates OpenAPI Standard 3.0 documentation endpoint /v3/**
+        return new Docket(DocumentationType.OAS_30)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
---
+++ backend/src/main/java/com/db/cloudschool/employeefeedback/security/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.db.cloudschool.employeefeedback.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // Disable cross-site request forgery protection.
+        http.csrf().disable();
+        http.authorizeRequests()
+                // Allow all Swagger URLs including OpenApi v3 docs
+                .antMatchers("/swagger-ui/**").permitAll()
+                .antMatchers("/swagger-resources/**").permitAll()
+                .antMatchers("/v3/**").permitAll()
+                // Allow the rest of URLs to be accessed only by authenticated users.
+                .antMatchers("/**").authenticated();
+        return http.build();
+    }
+}
