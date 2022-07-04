78e6ae7fc3f9df751a5fd788f26beb5c8044a74e
laur0700 <laur.postl@gmail.com>
2022-07-04 17:47:19 +0300

    refactored AuthController.java

--- backend/src/main/java/com/db/cloudschool/employeefeedback/controller/AuthController.java
+++ backend/src/main/java/com/db/cloudschool/employeefeedback/controller/AuthController.java
@@ -1,26 +1,29 @@
 package com.db.cloudschool.employeefeedback.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import com.db.cloudschool.employeefeedback.payload.request.AccessTokenRequest;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @RestController
 public class AuthController {
     //TODO: inject service here and refactor
 
     @PostMapping(path = "/login")
-    public void login(@RequestBody CredentialsDTO credentialsDTO){
+    public void login(@RequestBody LoginDTO loginDTO){
     }
 
-    @PostMapping(path = "/register")
-    public void register(@RequestBody CredentialsDTO credentialsDTO){
+    @RequestMapping(path = "/register", method = POST, consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    public void register(@RequestPart RegisterDTO data, @RequestPart MultipartFile profilePhoto){
     }
 
     @PostMapping(path = "/auth/access-token")
-    public void login(@RequestBody AccessTokenDTO accessTokenDTO){
+    public void getAccessToken(@RequestBody AccessTokenRequest accessTokenRequest){
     }
 
     @PostMapping(path = "/auth/invalid-date-refresh-token")
-    public void login(){
+    public void invalidateAccessToken(){
     }
 }
